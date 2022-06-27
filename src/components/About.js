6242896b43034b5c0abe9b19b0a3ff927772942d
Aaron Harris <Aaron.earl.harris@me.com>
2022-06-27 13:30:20 -0700

    changed header background photo and song

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,7 @@ const About = () => (
 
           </p>
         </h4>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/SlpzUInKZsw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/5opQ98WOhLo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <div className="row">
           <div className="columns contact-details">
             <h2>Contact Details</h2>
